b27ab7a3784e3437a36d791252d02b16d889f6e7
Patrick Tedeschi <patrick@tedeschi.com.br>
2016-01-24 12:56:41 -0200

    Rrestructuring folders to better organize the project

--- tools/NuGet/CreatePackage.ps1
+++ tools/NuGet/CreatePackage.ps1
@@ -4,5 +4,5 @@ Remove-Item *.nupkg
 # Create new packages for any nuspec files that exist in this directory.
 Foreach ($nuspec in $(Get-Item *.nuspec))
 {
-	.\NuGet.exe pack "$nuspec"
+	.\NuGet.exe pack "$nuspec" -Build -Properties Configuration=Release
 }
