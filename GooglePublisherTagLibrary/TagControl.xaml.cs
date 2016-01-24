284598932c0cde65e8bb9690589ee4be6f2819e5
Patrick Tedeschi <patrick@tedeschi.com.br>
2016-01-24 12:43:47 -0200

    Beginning project restructure to respect package libraries format

--- GooglePublisherTagLibrary/TagControl.xaml.cs
+++ GooglePublisherTagLibrary/TagControl.xaml.cs
@@ -22,20 +22,19 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 //-----------------------------------------------------------------------
-
-namespace GooglePublisherTag.Control
+namespace GooglePublisherTag
 {
     using System;
     using Windows.UI.Xaml.Controls;
 
-    public sealed partial class GooglePublisherTag : UserControl
+    public sealed partial class TagControl : UserControl
     {
         /// <summary>
         /// This is a sample class
         /// </summary>
         private const string RawData = "<html><head><script type='text/javascript'>var googletag = googletag || {};googletag.cmd = googletag.cmd || [];(function() {var gads = document.createElement('script');gads.async = true;gads.type = 'text/javascript';var useSSL = 'https:' == document.location.protocol;gads.src = (useSSL ? 'https:' : 'http:') +'//www.googletagservices.com/tag/js/gpt.js';var node = document.getElementsByTagName('script')[0];node.parentNode.insertBefore(gads, node);})();googletag.cmd.push(function() {googletag.defineSlot('[##ADUNIT##]', [[##WIDTH##], [##HEIGHT##]], 'div-google-ads').addService(googletag.pubads());googletag.pubads().enableSingleRequest();googletag.enableServices();});</script><style type='text/css'>body {margin:0;border: none;overflow:hidden;}</style></head><body><div id='div-google-ads' style='width:[##WIDTH##]px;height:[##HEIGHT##]px;'><script type='text/javascript'>googletag.cmd.push(function() {googletag.display('div-google-ads');});</script></div></body></html>";
 
-        public GooglePublisherTag()
+        public TagControl()
         {
             this.InitializeComponent();
         }
